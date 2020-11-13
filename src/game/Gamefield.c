4d92ec9e7e2b23b3e08101f6e56ffed035891709
Vitaliy <mr.prikota@gmail.com>
2020-11-14 01:16:19 +0300

    Added and implemented OnUpdateGamefield method

--- src/game/Gamefield.c
+++ src/game/Gamefield.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <glad/glad.h>
 
+uint8_t * GetRawColor32Array(Gamefield *gamefield);
+
 void InitGamefield(Gamefield* gamefield) {
     gamefield->width = WIDTH;
     gamefield->height = HEIGHT;
@@ -15,6 +17,7 @@ void InitGamefield(Gamefield* gamefield) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
+    OnUpdateGamefield(gamefield);
     UnBindGamefield(gamefield);
 }
 
@@ -26,3 +29,31 @@ void BindGamefield(uint32_t slot, Gamefield* gamefield) {
     glActiveTexture(GL_TEXTURE0 + slot);
     glBindTexture(GL_TEXTURE_2D, gamefield->rendererId);
 }
+
+void OnUpdateGamefield(Gamefield* gamefield) {
+    for (size_t i = 0; i < gamefield->height * gamefield->width; i++) {
+        Pixel pixel;
+        pixel.color.a = 1;
+        pixel.color.r = (rand() / RAND_MAX) * 255;
+        pixel.color.g = (rand() / RAND_MAX) * 255;
+        pixel.color.b = (rand() / RAND_MAX) * 255;
+        gamefield->pixels[i] = pixel;
+    }
+
+    uint8_t * rawPixelArray = GetRawColor32Array(gamefield);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, gamefield->width, gamefield->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, rawPixelArray);
+    free(rawPixelArray);
+}
+
+uint8_t * GetRawColor32Array(Gamefield *gamefield) {
+    uint8_t * pixelArray = malloc(gamefield->width * gamefield->height * 4);
+    size_t pixelArrayIndex = 0;
+    for (int i = 0; i < gamefield->width * gamefield->height; i++) {
+        pixelArray[pixelArrayIndex++] = gamefield->pixels[i].color.r;
+        pixelArray[pixelArrayIndex++] = gamefield->pixels[i].color.g;
+        pixelArray[pixelArrayIndex++] = gamefield->pixels[i].color.b;
+        pixelArray[pixelArrayIndex++] = gamefield->pixels[i].color.a;
+    }
+    return pixelArray;
+}
+
